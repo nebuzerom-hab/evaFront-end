b4bb2dc4e343c6e02ce2b4d49a9ca2106f423421
nebuzerom <nebuzeromk@gmail.com>
2025-05-22 08:03:11 -0400

    Fix Vite config with base path for GitHub Pages

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -1,11 +1,14 @@
-// import { defineConfig } from 'vite'
+// vite.config.js
+
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/evaFront-end/", // Important: matches your repo name
+  base: "/evaFront-end/",
   build: {
-    chunkSizeWarningLimit: 1000, // optional
+    chunkSizeWarningLimit: 1000,
   },
   server: {
     open: true,
