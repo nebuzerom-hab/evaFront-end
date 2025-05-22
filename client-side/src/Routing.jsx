ae42f8da41e2aaf45913b7a7fdc05e4be0db69ea
nebuzerom <nebuzeromk@gmail.com>
2025-05-22 07:31:43 -0400

    fixed path

--- client-side/src/Routing.jsx
+++ client-side/src/Routing.jsx
@@ -52,7 +52,8 @@ import Login from "./pages/Login";
 import Layout from "./components/Layout/Layout";
 import AskQuestion from "./components/AskQuestion/AskQuestion";
 import SingleQuestion from "./components/Question/SingleQuestion";
-import backgroundImage from "./assets/image/evaBG.png";
+import backgroundImage  from './assets/image/evaBg.png';
+
 import EditQuestionPage from "./components/Question/EditQuestion";
 import ProtectedRoute from "./components/ProtectedRoute/ProtectedRoute";
 import EditAnswer from "./components/AnswerForm/EditAnswer";
