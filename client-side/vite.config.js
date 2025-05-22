57ad46fc66da081f5b02c58f83635a0e2ea68a36
nebuzerom <nebuzeromk@gmail.com>
2025-05-22 07:57:49 -0400

    Set base path for GitHub Pages deployment

--- client-side/vite.config.js
+++ client-side/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from "vite";
+// import { defineConfig } from 'vite'
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/evaFront-end/", // Important: matches your repo name
+  build: {
+    chunkSizeWarningLimit: 1000, // optional
+  },
+  server: {
+    open: true,
+  },
 });
